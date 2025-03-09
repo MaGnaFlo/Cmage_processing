23effc8a4dc19a88196306a71bcb6ba6c04c6e3b
Florian Lardeux <magnaflo35@gmail.com>
2025-03-09 20:22:22 +0100

    Added some basic matrix operations

--- include/utils/matrix.h
+++ include/utils/matrix.h
@@ -2,9 +2,68 @@
 
 /// @brief Matrix structure
 typedef struct Matrix {
-    int width;
-    int height;
+    unsigned int width;
+    unsigned int height;
     double *data;
 } Matrix;
 
-extern Matrix create_matrix(int width, int height, double data[width][height]);
+extern Matrix zero_matrix(unsigned int width, unsigned int height);
+
+/// @brief Creates a matrix given its width, height and data array
+/// @param width Matrix width
+/// @param height Matrix height
+/// @param data Array of lines
+/// @return Created matrix
+extern Matrix create_matrix(unsigned int width, unsigned int height, double data[width][height]);
+
+/// @brief Returns value of mat at (i,j)
+/// @param mat Matrix
+/// @param i Row
+/// @param j Column
+/// @return Value at (i,j)
+extern double matrix_at(Matrix *mat, unsigned int i, unsigned int j);
+
+/// @brief Sets matrix value
+/// @param mat Matrix
+/// @param i Row
+/// @param j Column
+/// @param val Value
+extern void set_matrix_at(Matrix *mat, unsigned int i, unsigned int j, double val);
+
+/// @brief Multiplies two matrices
+/// @param A Left matrix
+/// @param B Right matrix
+/// @return Resulting matrix
+extern Matrix matmul(Matrix *A, Matrix *B);
+
+/// @brief Returns the transpose of a matrix
+/// @param mat Original matrix
+/// @return Transpose matrix
+extern Matrix transpose(Matrix *mat);
+
+/// @brief Returns the submatrix of mat, excluding row i and col j
+/// @param mat Matrix
+/// @param i_ex Excluded row
+/// @param j_ex Excluded col
+/// @return Submatrix
+extern Matrix submatrix(Matrix *mat, unsigned int i_ex, unsigned int j_ex);
+
+/// @brief Returns the comatrix (or matrix of cofactors)
+/// @param mat Original matrix
+/// @return Comatrix
+extern Matrix comatrix(Matrix *mat);
+
+/// @brief Returns the determinant of a matrix
+/// @param mat Matrix
+/// @return Determinant value
+extern double determinant(Matrix *mat);
+
+/// @brief Returns the inverse of a matrix
+/// @note If the matrix can't be transpose, returns a empty matrix
+/// @param mat Original matrix
+/// @return Inverse matrix
+extern Matrix inverse(Matrix *mat);
+
+/// @brief Pretty-print a matrix
+/// @param mat Matrix
+extern void print_matrix(Matrix *mat);
--- src/utils/matrix.c
+++ src/utils/matrix.c
@@ -2,28 +2,43 @@
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
-/// @brief Instanciates a matrix
-/// @param width Matrix width
-/// @param height Matrix height
-/// @param data Matrix array representation
-/// @return 
-Matrix create_matrix(int width, int height, double data[width][height])
+Matrix zero_matrix(unsigned int width, unsigned int height)
 {
     Matrix mat;
     mat.width = width;
     mat.height = height;
     mat.data = (double *)malloc(width * height * sizeof(double));
+    return mat;
+}
+
+Matrix create_matrix(unsigned int width, unsigned int height, double data[width][height])
+{
+    Matrix mat = zero_matrix(width, height);
     for (int j = 0; j < height; ++j) {
         memcpy(mat.data + j * width, data[j], width * sizeof(double));
     }
     return mat;
 }
 
-/// @brief Matrix multiplication
-/// @param A Left matrix
-/// @param B Right matrix
-/// @return
+double matrix_at(Matrix *mat, unsigned int i, unsigned int j)
+{
+    if (i >= mat->width || j >= mat->height) {
+        fprintf(stderr, "Accessing matrix value of out bounds (%d %d for size %dx%d)", i, j, mat->width, mat->height);
+        return 0;
+    }
+    return *(mat->data + i * mat->width + j);
+}
+
+void set_matrix_at(Matrix *mat, unsigned int i, unsigned int j, double val)
+{
+    if (i >= mat->width || j >= mat->height) {
+        fprintf(stderr, "Accessing matrix value of out bounds (%d %d for size %dx%d)", i, j, mat->width, mat->height);
+    }
+    *(mat->data + i * mat->width + j) = val;
+}
+
 Matrix matmul(Matrix *A, Matrix *B)
 {
     Matrix res;
@@ -34,15 +49,101 @@ Matrix matmul(Matrix *A, Matrix *B)
     res.width = B->width;
     res.height = A->height;
     res.data = (double *)malloc(res.width * res.height * sizeof(double));
-
+    
     for (int i = 0; i < A->height; ++i) {
         for (int j = 0; j < B->width; ++j) {
             double value = 0;
             for (int k = 0; k < res.height; ++k) {
-                value += *(A->data + i * A->width + k) * *(B->data + k * B->width + j);
+                value += matrix_at(A, i, k) * matrix_at(B, k, j);
             }
-            *(res.data + i * res.width + j) = value;
+            set_matrix_at(&res, i, j, value);
         }
     }
     return res;
+}
+
+Matrix submatrix(Matrix *mat, unsigned int i_ex, unsigned int j_ex)
+{
+    Matrix submat = zero_matrix(mat->width-1, mat->height-1);
+    unsigned int i_sub = 0;
+    for (int i = 0; i < mat->width; ++i) {
+        unsigned int j_sub = 0;
+        if (i == i_ex) continue;
+        for (int j = 0; j < mat->height; ++j) {
+            if (j == j_ex) continue;
+            set_matrix_at(&submat, i_sub, j_sub, matrix_at(mat, i, j));
+            ++j_sub;
+        }
+        ++i_sub;
+    }
+    return submat;
+}
+
+double determinant(Matrix *mat)
+{
+    double det_value = 0;
+    if (mat->width == 2) {
+        det_value = matrix_at(mat, 0, 0) * matrix_at(mat, 1, 1) - matrix_at(mat, 0, 1) * matrix_at(mat, 1, 0);
+    } else {
+        for (int i = 0; i < mat->height; ++i) {
+            Matrix submat = submatrix(mat, i, 0);
+            det_value += pow(-1, i) * matrix_at(mat, i, 0) * determinant(&submat);
+        }
+    }
+    return det_value;
+}
+
+Matrix comatrix(Matrix *mat)
+{
+    Matrix comat = zero_matrix(mat->width, mat->height);
+    for (int i = 0; i < mat->width; ++i) {
+        for (int j = 0; j < mat->height; ++j) {
+            Matrix submat = submatrix(mat, i, j);
+            set_matrix_at(&comat, i, j, pow(-1, i+j) * determinant(&submat));
+        }
+    }
+    return comat;
+}
+
+Matrix transpose(Matrix *mat)
+{
+    Matrix t_mat = zero_matrix(mat->width, mat->height);
+    for (int i = 0; i < mat->width; ++i) {
+        for (int j = 0; j < mat->height; ++j) {
+            set_matrix_at(&t_mat, i, j, matrix_at(mat, j, i));
+        }
+    }
+    return t_mat;
+}
+
+Matrix inverse(Matrix *mat)
+{
+    Matrix inverse;
+    double det = determinant(mat);
+    if (det == 0) {
+        perror("Matrix has a null determinant; inverse matrix cannot be computed.");
+        inverse.width = 0;
+        inverse.height = 0;
+        return inverse;
+    }
+
+    inverse = zero_matrix(mat->width, mat->height);
+    Matrix comat = comatrix(mat);
+    Matrix t_comat = transpose(&comat);
+    for (int i = 0; i < inverse.width; ++i) {
+        for (int j = 0; j < inverse.height; ++j) {
+            set_matrix_at(&inverse, i, j, matrix_at(&t_comat, i, j) / det);
+        }
+    }
+    return inverse;
+}
+
+void print_matrix(Matrix *mat)
+{
+    printf("Size: (%d,%d)\n", mat->width, mat->height);
+    for (int i = 0; i<mat->width; i++) {
+        for (int j = 0; j<mat->height; j++)
+        printf("%f ", matrix_at(mat, i, j));
+        printf("\n");
+    }
 }
